e162264775eae4d84496f365ef32ec33c3bc64d5
Phil Sturgeon <email@philsturgeon.co.uk>
2010-07-21 10:59:49 +0100

    Added security policy and a way to assign specific CodeIgniter libraries to syntax. Can someone test security policies actually do anything? Seems to be some bugs in Dwoo.

--- libraries/MY_Parser.php
+++ libraries/MY_Parser.php
@@ -26,37 +26,38 @@
  */
 include(APPPATH.'libraries/dwoo/dwooAutoload.php');
 
-class MY_Parser extends CI_Parser {
-    
-	private $ci;
-	
-	private $dwoo;
-	
+class MY_Parser extends CI_Parser
+{
+	private $_ci;
+
+	private $_dwoo;
+	private $_config;
+
 	function __construct()
 	{
-	 	$this->ci =& get_instance();
-	 	
-		$this->ci->config->load('parser', TRUE);
-        $config = $this->ci->config->item('parser');
-        
+	 	$this->_ci =& get_instance();
+
+		$this->_ci->config->load('parser', TRUE);
+        $this->_config = $this->_ci->config->item('parser');
+
         // Main Dwoo object
-        $this->dwoo = new Dwoo;
+        $this->_dwoo = new Dwoo;
 
          // The directory where compiled templates are located
-		$this->dwoo->setCompileDir( $config['parser_compile_dir'] );
-		$this->dwoo->setCacheDir( $config['parser_cache_dir'] );
-		$this->dwoo->setCacheTime( $config['parser_cache_time'] );
-		
+		$this->_dwoo->setCompileDir( $this->_config['parser_compile_dir'] );
+		$this->_dwoo->setCacheDir( $this->_config['parser_cache_dir'] );
+		$this->_dwoo->setCacheTime( $this->_config['parser_cache_time'] );
+
 		// Security
 		$security = new Dwoo_Security_Policy;
-		
-		$security->setPhpHandling($config['parser_allow_php_tags']);
-		$security->allowPhpFunction($config['parser_allowed_php_functions']);
-		
-		$this->dwoo->setSecurityPolicy( $security );
-		
+
+		$security->setPhpHandling($this->_config['parser_allow_php_tags']);
+		$security->allowPhpFunction($this->_config['parser_allowed_php_functions']);
+
+		$this->_dwoo->setSecurityPolicy( $security );
+
 	}
-	
+
 	/**
 	 *  Parse a view file
 	 *
@@ -71,11 +72,11 @@ class MY_Parser extends CI_Parser {
 	 */
 	function parse($template, $data = array(), $return = FALSE)
 	{
-		$string = $this->ci->load->view($template, $data, TRUE);
-		
-		return $this->_parse($string, $data, $return);	
+		$string = $this->_ci->load->view($template, $data, TRUE);
+
+		return $this->_parse($string, $data, $return);
 	}
-	
+
 	/**
 	 *  String parse
 	 *
@@ -92,7 +93,7 @@ class MY_Parser extends CI_Parser {
 	{
 		return $this->_parse($string, $data, $return);
     }
-	
+
 	/**
 	 *  Parse
 	 *
@@ -108,56 +109,61 @@ class MY_Parser extends CI_Parser {
 	function _parse($string, $data, $return = FALSE)
 	{
         // Start benchmark
-        $this->ci->benchmark->mark('dwoo_parse_start');
-        
+        $this->_ci->benchmark->mark('dwoo_parse_start');
+
 		// Compatibility with PyroCMS v0.9.7 style links
 		// TODO: Remove this for v1.0
 		$string = preg_replace('/\{page_url\[([0-9]+)\]\}/', '{page_url($1)}', $string);
-		
-        // Object containing data
-        $dwoo_data = new Dwoo_Data();
-        
+
         // Convert from object to array
-        if(is_object($data))
+        if(!is_array($data))
         {
         	$data = (array) $data;
         }
-        
-        // If not an empty array, set data
-        if(is_array($data) && $data !== array())
-        {
-	        $dwoo_data->setData($data);
-        }
-        
+
+        $data = array_merge($data, $this->_ci->load->_ci_cached_vars);
+
+		foreach ($this->_config['parser_assign_refs'] as $ref)
+		{
+	        $data[$ref] =& $this->_ci->{$ref};
+		}
+
+		// TODO: DEPRECATED kill ci value
+		//$data['ci'] =& $this->_ci;
+
+        // Object containing data
+        $dwoo_data = new Dwoo_Data;
+        $dwoo_data->setData($data);
+
         try
         {
 	        // Object of the template
 	        $tpl = new Dwoo_Template_String($string);
-	        
+
 	        // render the template
-	        $parsed_string = $this->dwoo->get($tpl, $dwoo_data);
+	        $parsed_string = $this->_dwoo->get($tpl, $dwoo_data);
         }
-        
+
         catch(Dwoo_Compilation_Exception $e)
         {
         	show_error($e);
         }
-        
+
         // Finish benchmark
-        $this->ci->benchmark->mark('dwoo_parse_end');
+        $this->_ci->benchmark->mark('dwoo_parse_end');
 
         // Return results or not ?
 		if ( !$return )
 		{
-			$this->ci->output->append_output($parsed_string);
+			$this->_ci->output->append_output($parsed_string);
 			return;
 		}
-		
+
 		return $parsed_string;
 	}
-	
+
 	// --------------------------------------------------------------------
-	
+
 }
 // END MY_Parser Class
 
